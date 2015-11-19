6d1cbd4821aa900abcf9651ae19e1e508d35a7b7
Stephanie Tong <tong.stephanie_mylinh@yahoo.com>
2015-11-18 17:06:05 -0800

    Got animated Square to work

--- master/master.ino
+++ master/master.ino
@@ -1,11 +1,14 @@
 #include <LedControlMS.h>
 #include "Agenda.h"
 #include "keypad.h"
+#include "patterns.h"
 #include "lcd_messages.h"
 #include <avr/io.h>
 
 // ================ GLOBALS ========================
 #define ONE_SEC 1000000
+#define TASK_LEDMAT_PERIOD 100000
+#define FRAME_TIME 500000
 #define MIN_OPTION 1
 #define MAX_OPTION 4
 #define MOSI_PIN 51
@@ -17,6 +20,9 @@
 enum rowNum {MROW1, MROW2, MROW3, MROW4, MROW5, MROW6, MROW7, MROW8};
 enum colNum {MCOL1, MCOL2, MCOL3, MCOL4, MCOL5, MCOL6, MCOL7, MCOL8};
 
+char key;
+int menuOption;
+
 // ================ SCHEDULER ========================
 Agenda scheduler;
 int task1, task2;
@@ -24,20 +30,53 @@ int task1, task2;
 // ================ LEDCONTROL ========================
 // constructor parameters: dataPin, clkPin, csPin, numDevices)
 LedControl lc = LedControl(MISO_PIN, SCK_PIN, SS_LEDMAT, 1);
+
+int frameIndex = 0;
+long frameTime = FRAME_TIME;
+
 void LedControl_init()
 {
   lc.shutdown(LEDMAT_ADDR, false);
-  lc.setIntensity(LEDMAT_ADDR, 8);
+  lc.setIntensity(LEDMAT_ADDR, 6);
   lc.clearDisplay(LEDMAT_ADDR);
-  //lc.setRow(LEDMAT_ADDR, MROW2, B10000001); 
+}
+
+void displayPattern(const Pattern& p)
+{
+  lc.clearDisplay(LEDMAT_ADDR);
+
+  for (int i = 0; i < 8; i++)
+    lc.setRow(LEDMAT_ADDR, i, p.row[i]);
+}
+
+void animateFrames()
+{
+  // if frameTime > 0, stay on same frame. Decrement frameTime
+  if (frameTime > 0) 
+  {
+    displayPattern(Frames[frameIndex]);
+    frameTime -= TASK_LEDMAT_PERIOD;
+  }
+
+  // otherwise, it's time to move onto the next frame
+  else
+  {
+    frameTime = FRAME_TIME;
+    
+    if (frameIndex != numFrames-1)
+      frameIndex++;
+
+    else
+      frameIndex = 0;
+  }  
+  
 }
 
 // ================ TASK MAIN ========================
 enum T1_SM {MainInit, MainMenu, DrawMode, SyncMode, Reset} mainState;
 void Task_Main()
 {
-  static int menuOption;
-  char key = GetKeypadKey();
+  key = GetKeypadKey();
 
   switch (mainState)
   {
@@ -95,23 +134,59 @@ void Task_Main()
 
 }
 
-void Task_Blink()
+enum T2_SM {Idle, Drawing, Playing, Syncing} ledState;
+void Task_LedMat()
 {
-  //Serial.println("Blink!");
+  
+  // transitions
+  switch (ledState)
+  {
+    case Idle:
+      if (key == '*')
+      {
+        frameIndex = 0;
+        frameTime = FRAME_TIME;
+        ledState = Playing;
+      }
+      break;
+    case Playing:
+      if (key == '*')
+        ledState = Idle;
+      break;
+    default:
+      ledState = Idle;
+  }
+
+  // actions
+  switch (ledState)
+  {
+    case Idle:
+      // do nothing
+      break;
+    case Playing:
+      animateFrames();
+      break;
+    default:
+      ledState = Idle;
+  }
 }
 
 void setup() {
   // put your setup code here, to run once:
 
    LCD_init(); // LCD data lines on PORTL
-   LedControl_init();
+   LedControl_init(); // init ledControl
+   Pattern_init();
    Keypad_init(); // Keypad on PortA
   
   Serial.begin(115200);
+  
+  mainState = MainInit;
   task1 = scheduler.insert(Task_Main, ONE_SEC/8, false);
   scheduler.activate(task1);
 
-  task2 = scheduler.insert(Task_Blink, ONE_SEC, false);
+  ledState = Idle;
+  task2 = scheduler.insert(Task_LedMat, TASK_LEDMAT_PERIOD, false);
   scheduler.activate(task2);
 }
 
--- master/patterns.h
+++ master/patterns.h
@@ -1,23 +1,86 @@
 #ifndef PATTERNS_H
 #define PATTERNS_H
 
+#define numFrames 3
+
 struct Pattern
 {
-  unsigned char patternFrames = 0;
+  byte row[8];
 
-  unsigned char row[3];
-  unsigned char col[3]; 
-
-  void displayPattern()
+  void clearPattern()
   {
-
+    for (int i = 0; i < 8; ++i)
+      row[i] = B00000000;
   }
 
-  void addPattern()
+  Pattern()
   {
-    
+    clearPattern();
+  }
+
+  Pattern& operator = (const Pattern& rhs)
+  {
+    if (this != &rhs)
+    {
+      for (int i = 0; i < 8; ++i)
+        row[i] = rhs.row[i];
+    }
   }
 
 };
+
+Pattern DefaultFrames[numFrames];
+Pattern Frames[numFrames];
+
+void Pattern_init()
+{
+  // pattern for the first frame;
+  Pattern f1;
+  f1.row[0] = B11111111;
+  f1.row[1] = B10000001;
+  f1.row[2] = B10000001;
+  f1.row[3] = B10000001;
+  f1.row[4] = B10000001;
+  f1.row[5] = B10000001;
+  f1.row[6] = B10000001;
+  f1.row[7] = B11111111;
+
+  // pattern for the second frame
+  Pattern f2;
+  f2.row[0] = B00000000;
+  f2.row[1] = B01111110;
+  f2.row[2] = B01000010;
+  f2.row[3] = B01000010;
+  f2.row[4] = B01000010;
+  f2.row[5] = B01000010;
+  f2.row[6] = B01111110;
+  f2.row[7] = B00000000;
+
+  // pattern for third frame
+  Pattern f3;
+  f3.row[0] = B00000000;
+  f3.row[1] = B00000000;
+  f3.row[2] = B00111100;
+  f3.row[3] = B00100100;
+  f3.row[4] = B00100100;
+  f3.row[5] = B00111100;
+  f3.row[6] = B00000000;
+  f3.row[7] = B00000000;
+
+  DefaultFrames[0] = f1;
+  DefaultFrames[1] = f2;
+  DefaultFrames[2] = f3;
+
+  Frames[0] = f1;
+  Frames[1] = f2;
+  Frames[2] = f3;
+}
+
+void resetFrames()
+{
+  for (int i = 0; i < numFrames; ++i)
+    Frames[i] = DefaultFrames[i];
+}
+
 
 #endif
